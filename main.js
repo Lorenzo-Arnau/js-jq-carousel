02a35af75e30acb6660131f4049b603866126101
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-13 15:52:09 +0100

    update,now works keyboards arrow and click on spot

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// funzione ready documento
 $(document).ready(function(){
 
+// funzione on click freccia sinistra carousel
 $('.prev').click(function(){
   $('img.active').removeClass('active').prev().addClass('active');
   $('i.active').removeClass('active').prev().addClass('active');
@@ -10,7 +12,7 @@ $('.prev').click(function(){
     $('i.last').addClass('active');
   }
 });
-
+// funzione on click freccia destra carousel
   $('.next').click(function(){
     $('img.active').removeClass('active').next().addClass('active');
     $('i.active').removeClass('active').next().addClass('active');
@@ -20,8 +22,34 @@ $('.prev').click(function(){
       $('i.last').removeClass('active')
       $('i.first').addClass('active');
     }
-  })
+  });
 
+// funzione on click frecce tastiera
+  $("body").keydown(function(e) {
+    if(e.keyCode == 37) { // sinistra
+      $('img.active').removeClass('active').prev().addClass('active');
+      $('i.active').removeClass('active').prev().addClass('active');
+      if ($('img.active').hasClass('first') === true) {
+        $('img.first').removeClass('active');
+        $('img.last').addClass('active');
+        $('i.first').removeClass('active')
+        $('i.last').addClass('active');
+      }
+    }
+    if(e.keyCode == 39) { // destra
+      $('img.active').removeClass('active').next().addClass('active');
+      $('i.active').removeClass('active').next().addClass('active');
+      if ($('img.active').hasClass('last') === true) {
+        $('img.last').removeClass('active');
+        $('img.first').addClass('active');
+        $('i.last').removeClass('active')
+        $('i.first').addClass('active');
+      }
+    }
+  });
+
+
+  // funzione click sui pallini per vedere le immagini
   $('.nav i:first-child').click(function(){
     $('img.active').removeClass('active');
     $('.images img:first-child').addClass('active');
