2d272a9f519552cf82ae3f2a4dffb2d48ea30a15
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-14 10:27:43 +0100

    update

--- main.js
+++ main.js
@@ -3,63 +3,46 @@ $(document).ready(function(){
 
 // funzione on click freccia sinistra carousel
 $('.prev').click(function(){
-  var imgAttiva = $('img.active') ;
-  var iAttiva = $('i.active') ;
-  imgAttiva.removeClass('active');
-  iAttiva.removeClass('active');
-  if (imgAttiva.hasClass('first') === true) {
-      $('img.last').addClass('active');
-      $('i.last').addClass('active');
+  var attiva = $('.active') ;
+  attiva.removeClass('active');
+  if (attiva.hasClass('first') === true) {
+      $('.last').addClass('active');
     }else {
-    imgAttiva.prev().addClass('active');
-    iAttiva.prev().addClass('active');
+    attiva.prev().addClass('active');
   }
 });
 // funzione on click freccia destra carousel
   $('.next').click(function(){
-    var imgAttiva = $('img.active') ;
-    var iAttiva = $('i.active') ;
-    imgAttiva.removeClass('active');
-    iAttiva.removeClass('active');
-    if (imgAttiva.hasClass('last') === true) {
-      $('img.first').addClass('active');
-      $('i.first').addClass('active');
+    var attiva = $('.active') ;
+    attiva.removeClass('active');
+    if (attiva.hasClass('last') === true) {
+      $('.first').addClass('active');
     }else {
-      imgAttiva.next().addClass('active');
-      iAttiva.next().addClass('active');
+      attiva.next().addClass('active');
     }
   });
 
 // funzione on click frecce tastiera
   $("body").keydown(function(e) {
     if(e.keyCode == 37) { // sinistra
-      var imgAttiva = $('img.active') ;
-      var iAttiva = $('i.active') ;
-      imgAttiva.removeClass('active');
-      iAttiva.removeClass('active');
-      if (imgAttiva.hasClass('first') === true) {
-          $('img.last').addClass('active');
-          $('i.last').addClass('active');
+      var attiva = $('.active') ;
+      attiva.removeClass('active');
+      if (attiva.hasClass('first') === true) {
+          $('.last').addClass('active');
         }else {
-        imgAttiva.prev().addClass('active');
-        iAttiva.prev().addClass('active');
+        attiva.prev().addClass('active');
       }
     }
     if(e.keyCode == 39) { // destra
-        var imgAttiva = $('img.active') ;
-        var iAttiva = $('i.active') ;
-        imgAttiva.removeClass('active');
-        iAttiva.removeClass('active');
-        if (imgAttiva.hasClass('last') === true) {
-          $('img.first').addClass('active');
-          $('i.first').addClass('active');
-        }else {
-          imgAttiva.next().addClass('active');
-          iAttiva.next().addClass('active');
-        }
-      }    
+      var attiva = $('.active') ;
+      attiva.removeClass('active');
+      if (attiva.hasClass('last') === true) {
+        $('.first').addClass('active');
+      }else {
+        attiva.next().addClass('active');
+      }
+    }
   });
-
 
   // funzione click sui pallini per vedere le immagini
   $('.nav i:first-child').click(function(){
@@ -90,7 +73,5 @@ $('.prev').click(function(){
     $(this).addClass('active');
 
   })
-
-
 
 });
